0d9c2ba107e568d1165ff75312a9982f1e661683
Zahra Vasanji <zahravasanji@hotmail.com>
2024-06-24 15:33:07 +0100

    added comments

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import "./App.css";
 import Sort from "./components/Sort";
 
 const App = () => {
-  // const [cartProducts, setCartProducts] = useState([])
   const [data, setData] = useState([]);
 
+  //get data and format as required
   const getData = async () => {
     try {
       const data = await axios.get("https://fakestoreapi.com/products/");
@@ -22,16 +22,10 @@ const App = () => {
     }
   };
 
+  //useEffect to run func
   useEffect(() => getData, []); //double check
 
-  const getTotalLiked = () => {
-    let _data = [...data];
-    _data = _data.filter((item) => {
-      return item.liked;
-    });
-    return _data.length;
-  };
-
+  //funcs are sent to children as callbacks sending back ids
   const onLike = (id) => {
     const _data = [...data];
     const index = _data.findIndex((data) => {
@@ -41,6 +35,16 @@ const App = () => {
     setData(_data);
   };
 
+  const getTotalLiked = () => {
+    let _data = [...data];
+    _data = _data.filter((item) => {
+      return item.liked;
+    });
+    return _data.length;
+  };
+
+
+//add prop of added:true and qty:1 to data obj or increment qty if it already exists. update state with new list
   const onAdd = (id) => {
     let _data = [...data];
     const index = _data.findIndex((item) => {
@@ -52,9 +56,10 @@ const App = () => {
     } else {
       _data[index].quantity = _data[index].quantity + 1;
     }
-
     setData(_data);
   };
+
+//determine which button was clicked, increment/decrement qty prop in obj. if qty is 1 and user wants to decrement, remove item from cart. update state
   const changeQuantity = (id, value) => {
     let _data = [...data];
     const index = _data.findIndex((item) => {
@@ -71,6 +76,9 @@ const App = () => {
     }
     setData(_data);
   };
+
+  //Get value for sort option and set state
+  //Keep getting warning about sort needing to return a func even though this already does. Why??
   const onSort = (value) => {
     const _data = [...data];
     const filteredData = _data.sort((a, b) => {
@@ -87,16 +95,18 @@ const App = () => {
           return -1;
         }
       }
-      return; //I thought this would clear the warning message but it doesn't?
+      return; 
     });
 
-    setData(filteredData); // check if this can be done without setting state?
+    setData(filteredData); // Can it be done without setting state again?
   };
 
+  //Show loading message if no data
   if (!data) {
-    return <h2>Hold tight, we're getting your products...</h2>; //only rendering if data exists - is this correct?
+    return <h2>Hold tight, we're getting your products...</h2>;
   }
 
+  //filter data for prop added:true and calculate totals and pass this data to Cart comp
   let totalQuantity = 0;
   let totalPrice = 0;
   let cartProducts = [...data];
