a2f9be2c87624a49d403473d3a4cf5247a8da8bd
Zahra Vasanji <zahravasanji@hotmail.com>
2024-08-28 15:16:39 +0100

    moved data

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
   Sort as SortIcon,
 } from "@mui/icons-material";
 import { Badge } from "@mui/material";
-import {pets} from "./pets.js";
+// import {pets} from "./pets.js";
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -20,6 +20,153 @@ const App = () => {
   const [showSort, setShowSort] = useState();
   const [showFilter, setShowFilter] = useState();
   const [cartVisible, setCartVisible] = useState(false);
+
+  const pets = [
+    {
+      id: "1",
+      title: "Sprinkles",
+      image: "/pets/gratisography-boxing-boxer-free-stock-photo.jpg",
+      category: "Dogs",
+      rating: 4.75,
+      price: "87"
+    },
+    {
+      id: "2",
+      title: "Tofu",
+      image: "/pets/gratisography-business-bunny-free-stock-photo.jpg",
+      category: "Rabbits",
+      rating: 1,
+      price: "102"
+    },
+    {
+      id: "3",
+      title: "Muffler",
+      image: "/pets/gratisography-cat-thief-stock-photo.jpg",
+      category: "Cats",
+      rating: 3.5,
+      price: "131"
+    },
+    {
+      id: "4",
+      title: "Spatula",
+      image: "/pets/gratisography-chill-chinchilla-free-stock-photo.jpg",
+      category: "Other",
+      rating: 2.18,
+      price: "77"
+    },
+    {
+      id: "5",
+      title: "Velcro",
+      image: "/pets/gratisography-cool-cat.jpg",
+      category: "Cats",
+      rating: 5,
+      price: "144"
+    },
+    {
+      id: "6",
+      title: "Nimbus",
+      image: "/pets/gratisography-cyber-kitty.jpg",
+      category: "Cats",
+      rating: 0.92,
+      price: "93"
+    },
+    {
+      id: "7",
+      title: "Bagel II",
+      image: "/pets/gratisography-flying-squirrel-free-stock-photo.jpg",
+      category: "Other",
+      rating: 4.4,
+      price: "68"
+    },
+    {
+      id: "8",
+      title: "Picklejuice",
+      image: "/pets/gratisography-frog-racer-free-stock-photo.jpg",
+      category: "Frogs",
+      rating: 2,
+      price: "115"
+    },
+    {
+      id: "9",
+      title: "Wigglebutt",
+      image: "/pets/gratisography-giraffe-portrait-free-stock-photo.jpg",
+      category: "Other",
+      rating: 1.67,
+      price: "72"
+    },
+    {
+      id: "10",
+      title: "Fang Jr",
+      image: "/pets/gratisography-kitten-chef-free-stock-photo.jpg",
+      category: "Cats",
+      rating: 3,
+      price: "139"
+    },
+    {
+      id: "11",
+      title: "Bullet",
+      image: "/pets/gratisography-kitten-plant-free-stock-photo.jpg",
+      category: "Cats",
+      rating: 4,
+      price: "110"
+    },
+    {
+      id: "12",
+      title: "Snuggles",
+      image: "/pets/gratisography-neon-frog-free-stock-photo.jpg",
+      category: "Frogs",
+      rating: 2.89,
+      price: "80"
+    },
+    {
+      id: "13",
+      title: "Simon",
+      image: "/pets/gratisography-not-happy-in-a-hat-stock-photo.jpg",
+      category: "Dogs",
+      rating: 0.5,
+      price: "134"
+    },
+    {
+      id: "14",
+      title: "Beaker",
+      image: "/pets/gratisography-parrot-pirate-free-stock-photo.jpg",
+      category: "Birds",
+      rating: 3.14,
+      price: "99"
+    },
+    {
+      id: "15",
+      title: "Zeus",
+      image: "/pets/gratisography-reindeer-dog.jpg",
+      category: "Dogs",
+      rating: 1,
+      price: "143"
+    },
+    {
+      id: "16",
+      title: "Dash",
+      image: "/pets/gratisography-sloth-breakfast-free-stock-photo.jpg",
+      category: "Other",
+      rating: 4.8,
+      price: "125"
+    },
+    {
+      id: "17",
+      title: "Pudding",
+      image: "/pets/gratisography-toadstool-free-stock-photo.jpg",
+      category: "Frogs",
+      rating: 2.55,
+      price: "67"
+    },
+    {
+      id: "18",
+      title: "Pancake",
+      image: "/pets/gratisography-wedding-dog-free-stock-photo.jpg",
+      category: "Dogs",
+      rating: 4.9,
+      price: "85"
+    }
+  ]
 
   //get data and format as required
   const getData = () => {
